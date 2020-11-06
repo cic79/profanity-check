c17c9023f8a046ac7a10d9c72b12389725ad283b
Federico Conte <federico.conte@quentral.com>
2020-11-06 18:55:37 +0100

    clean_data_it ready to be trained

--- profanity_check/data/generate_clean_data.py
+++ profanity_check/data/generate_clean_data.py
@@ -1,16 +1,17 @@
 import pandas as pd
-import numpy as np
 
-i = 0
-
-dtype = {'comment_text': str, 'toxic': np.bool, 'severe_toxic': np.bool, 'obscene': np.bool, 'threat': np.bool,
-         'insult': np.bool, 'identity_hate': np.bool}
+dtype = {'comment_text': str, 'toxic': bool, 'severe_toxic': bool, 'obscene': bool, 'threat': bool, 'insult': bool,
+         'identity_hate': bool}
 rows = pd.read_csv('jigsaw-toxic-comment-train-google-it-cleaned.csv', dtype=dtype)
 
+datas = []
+
 for row in rows.itertuples(index=False, name=None):
-    print(row)
-    comment = row[2]
-    print(comment)
-    i += 1
-    if i > 1:
-        break
+    comment_text = row[3]
+    # At least one between toxic, severe_toxic, obscene, threat, insult and identity_hate is True
+    is_offensive = int(bool(row[4:].count(True)))
+    datas.append((is_offensive, comment_text))
+
+columns = ['is_offensive', 'text']
+df = pd.DataFrame(data=datas, columns=columns)
+df.to_csv('clean_data_it.csv', index=False)
--- profanity_check/data/train_model.py
+++ profanity_check/data/train_model.py
@@ -4,7 +4,7 @@ from sklearn.calibration import CalibratedClassifierCV
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 
-data = pd.read_csv("clean_data.csv")
+data = pd.read_csv("clean_data_en.csv")
 texts = data["text"].astype(str)
 y = data["is_offensive"]
 
@@ -15,5 +15,19 @@ model = LinearSVC(class_weight="balanced", dual=False, tol=1e-2, max_iter=1e5)
 cclf = CalibratedClassifierCV(base_estimator=model)
 cclf.fit(X, y)
 
-dump(vectorizer, "vectorizer.joblib")
-dump(cclf, "model.joblib")
+dump(vectorizer, "vectorizer_en.joblib")
+dump(cclf, "model_en.joblib")
+
+data = pd.read_csv("clean_data_it.csv")
+texts = data["text"].astype(str)
+y = data["is_offensive"]
+
+vectorizer = TfidfVectorizer(stop_words="italian", min_df=0.0001)
+X = vectorizer.fit_transform(texts)
+
+model = LinearSVC(class_weight="balanced", dual=False, tol=1e-2, max_iter=1e5)
+cclf = CalibratedClassifierCV(base_estimator=model)
+cclf.fit(X, y)
+
+dump(vectorizer, "vectorizer_it.joblib")
+dump(cclf, "model_it.joblib")
